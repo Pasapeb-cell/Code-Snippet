5db22fa21ba86a4d5c5ea9dfe1a341d61c3142bb
Pablo Peralta <pablo.peralta@pinkerton.com>
2024-03-13 20:42:52 +0900

    Added basic chat functionality

--- webInterface.py
+++ webInterface.py
@@ -1,22 +1,19 @@
-from fastapi import FastAPI, WebSocket
-import uvicorn
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
 
-app = FastAPI()
+app = Flask(__name__)
+app.config['SECRET'] = 'secret!123'
+socketio = SocketIO(app, cors_allowed_origins='*')
 
+@socketio.on('message')
+def handle_message(message):
+    print(message)
+    if message != "User connected":
+        send(message, broadcast=True)
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-@app.websocket("/chat")
-async def chat(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        message = await websocket.receive_text()
-        # Process the received message
-        # Implement your chat logic here
-        # You can send messages back to the client using `await websocket.send_text(message)`
-        
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    socketio.run(app, host='localhost', use_reloader=False, log_output=False)
